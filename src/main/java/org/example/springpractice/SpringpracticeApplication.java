687651d0e8477532a24a4e1e0feea19ce78bcd73
Insoo-Hwang <iaminsoo@naver.com>
2024-06-23 13:05:13 +0900

    스프링 컨테이너로 통합

--- src/main/java/org/example/springpractice/SpringpracticeApplication.java
+++ src/main/java/org/example/springpractice/SpringpracticeApplication.java
@@ -10,31 +10,47 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.IOException;
 
+@Configuration //Bean 어노테이션을 가진 팩토리 메소드
 public class SpringpracticeApplication {
-
-    public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(); //Spring Container
-        applicationContext.registerBean(HelloController.class); //Bean 등록
-        applicationContext.registerBean(SimpleHelloService.class); //Bean 등록 + HelloController에 의존성 주입(HelloService 인터페이스를 구현한 것 중 유일)
-        applicationContext.refresh(); //Bean Object 생성
-
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //Tomcat이 아닌 Container 사용 가능
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                        new DispatcherServlet(applicationContext) //Servlet Container에 Spring Container 정보 전달
-                    ).addMapping("/*"); //Path의 모든 요청 처리
-        });
-        webServer.start(); //Tomcat Servlet Container 실행
+    @Bean
+    public HelloController helloController(HelloService helloService){
+        return new HelloController(helloService);
     }
 
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //Tomcat이 아닌 Container 사용 가능
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this) //Servlet Container에 Spring Container 정보 전달
+                    ).addMapping("/*"); //Path의 모든 요청 처리
+                });
+                webServer.start(); //Tomcat Servlet Container 실행
+            }
+        }; //Spring Container
+        applicationContext.register(SpringpracticeApplication.class); //Bean 등록
+        applicationContext.refresh(); //Bean Object 생성
+    }
 }
