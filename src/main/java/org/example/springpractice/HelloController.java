306f1ac29ae73367f74e883e188bb79ee35a50ea
Insoo-Hwang <iaminsoo@naver.com>
2024-06-23 12:52:20 +0900

    디스패처 서블릿

--- src/main/java/org/example/springpractice/HelloController.java
+++ src/main/java/org/example/springpractice/HelloController.java
@@ -1,9 +1,14 @@
 package org.example.springpractice;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Objects;
 
+@Controller //Spring boot 3.0부터 @Controller 명시
+@RequestMapping("/hello") //Dispatcher Servlet에 class 레벨 정보 전달(method 레벨까지 파악하기엔 Bean 정보가 너무 많음)
 public class HelloController {
     private final HelloService helloService;
 
@@ -11,6 +16,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping//Dispatcher Servlet에 매핑 정보 전달
+    @ResponseBody //String 타입으로 return하면 view를 확인하기 때문에 메시지를 명시적으로 return한다고 표기
     public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name)); //null 체크
     }
--- src/main/java/org/example/springpractice/SpringpracticeApplication.java
+++ src/main/java/org/example/springpractice/SpringpracticeApplication.java
@@ -15,39 +15,24 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.IOException;
 
 public class SpringpracticeApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext(); //Spring Container
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(); //Spring Container
         applicationContext.registerBean(HelloController.class); //Bean 등록
         applicationContext.registerBean(SimpleHelloService.class); //Bean 등록 + HelloController에 의존성 주입(HelloService 인터페이스를 구현한 것 중 유일)
         applicationContext.refresh(); //Bean Object 생성
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //Tomcat이 아닌 Container 사용 가능
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    //인증, 보안, 다국어, 공통 기능 처리
-                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { //hello 요청 처리 && GET 방식
-                        //======================요청 start======================
-                        String name = req.getParameter("name"); //queryString에서 추출
-                        //=======================요청 end=======================
-                        HelloController helloController = applicationContext.getBean(HelloController.class); //Bean을 가져옴
-                        String ret = helloController.hello(name); //hello+name 문자열 생성
-                        //======================응답 start======================
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE); //header 지정
-                        resp.getWriter().println(ret);
-                        //=======================응답 end=======================
-                    }
-                    else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value()); //나머지 페이지는 404 error
-                    }
-                }
-            }).addMapping("/*"); //Path의 모든 요청 처리
+            servletContext.addServlet("dispatcherServlet",
+                        new DispatcherServlet(applicationContext) //Servlet Container에 Spring Container 정보 전달
+                    ).addMapping("/*"); //Path의 모든 요청 처리
         });
         webServer.start(); //Tomcat Servlet Container 실행
     }
